163027456ec11138489cee38639cd401d03025f4
Max Euler <maxjuliuseuler@gmail.com>
2019-06-28 22:01:12 +0200

    next and react setup

---
+++ frontend/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <div>Logo</div>
+    <nav>Nav</nav>
+    <div>Search</div>
+    <nav>Acc</nav>
+  </header>
+);
+
+export default Header;
---
+++ frontend/pages/index.js
@@ -0,0 +1,7 @@
+const Home = () => (
+  <div>
+    <h3>Hello World</h3>
+  </div>
+);
+
+export default Home;
